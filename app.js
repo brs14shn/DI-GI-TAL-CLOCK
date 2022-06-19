c15eef0f3c0916dd07f8620c3e10fe8d9f46b03c
brs14shn <brsshn292929@gmail.com>
2022-06-19 17:43:44 +0300

    setInterval was added

---
+++ app.js
@@ -0,0 +1,34 @@
+
+setInterval(()=>{
+    let hours =document.getElementById("hours");
+    let minutes=document.getElementById("minutes");
+    let seconds=document.getElementById("seconds");
+    let ampm=document.getElementById("ampm");
+    
+    let h=new Date().getHours();
+    let m=new Date().getMinutes();
+    let s=new Date().getSeconds();
+    let am=h>=12 ? "PM":"AM"
+    
+    
+    //convert 24hr to 12hr clock
+    if(h>12){
+        h=h-12
+    }
+    
+    //* single digit add zero
+    h=(h<10) ? "0"+h :h;
+    m=(m<10) ? "0"+m :m;
+    s=(s<10) ? "0"+s :s;
+    
+    hours.innerHTML=h;
+    minutes.innerHTML=m;
+    seconds.innerHTML=s;
+    ampm.innerHTML=am;
+
+}
+,2000)
+
+
+
+
